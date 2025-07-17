4f6b26b6735ca596a487c6db65e6abf1ec0fa2a1
unknown <edwardespinel22@gmail.com>
2025-07-17 17:56:16 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className="text-2xl font-bold text-blue-600">Proyecto listo 🎉</div>
+  </React.StrictMode>
+);
